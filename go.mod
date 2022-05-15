3ef4f68191abd2c18e55f0873263ce5523942175
Guillaume <guillaume@ophane.net>
2022-05-16 00:04:55 +0200

    update

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/F0rth/lsd
 
 go 1.18
 
+require github.com/AllenDang/giu v0.6.3-0.20220509140256-40140524c877
+
 require (
-	github.com/AllenDang/giu v0.6.3-0.20220509140256-40140524c877 // indirect
 	github.com/AllenDang/go-findfont v0.0.0-20200702051237-9f180485aeb8 // indirect
 	github.com/AllenDang/imgui-go v1.12.1-0.20220322114136-499bbf6a42ad // indirect
 	github.com/faiface/mainthread v0.0.0-20171120011319-8b78f0a41ae3 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -11,11 +12,20 @@ import (
 
 var (
 	url              string
+	binary           string  = "chat_downloader"
 	splitLayoutPos   float32 = 320
 	size             int32   = 10
 	pasta            bool    = false
 	editor1, editor2 *g.CodeEditorWidget
+	f1, f2           *os.File
+	err1, err2       error
 )
+
+func saveTofile() {
+	os.WriteFile(f1.Name(), []byte(editor1.GetText()), 0666)
+	os.WriteFile(f2.Name(), []byte(editor2.GetText()), 0666)
+
+}
 
 func reset() {
 	editor1.Text("")
@@ -27,7 +37,7 @@ func scrap() {
 }
 
 func chat_download() {
-	cmd := exec.Command("chat_downloader", url)
+	cmd := exec.Command(binary, url)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -47,6 +57,7 @@ func chat_download() {
 				editor2.InsertText(live + "\n")
 			}
 		}
+		saveTofile()
 	}
 }
 func loop() {
@@ -61,8 +72,12 @@ func loop() {
 			g.Label("url:"),
 			g.InputText(&url).Size(g.Auto-300),
 			g.Button("scrap!").OnClick(scrap),
-			g.Button("oh crap").OnClick(reset),
+			//g.Button("oh crap").OnClick(reset),
 			g.Checkbox("pasta", &pasta),
+		),
+		g.Row(
+			g.Label("binary path:"),
+			g.InputText(&binary),
 		),
 		g.SliderInt(&size, 6, 40).Size(g.Auto),
 		g.SplitLayout(split_direction, &splitLayoutPos,
@@ -73,8 +88,26 @@ func loop() {
 }
 
 func main() {
+
 	editor1 = g.CodeEditor().Border(true).ShowWhitespaces(false).TabSize(2)
 	editor2 = g.CodeEditor().Border(true).ShowWhitespaces(false).TabSize(2)
+	f1, err1 = os.Open("members.txt")
+	if os.IsNotExist(err1) {
+		f1, _ = os.Create("members.txt")
+	}
+	file1, _ := os.ReadFile("members.txt")
+	editor1.InsertText(string(file1))
+
+	f2, err2 = os.Open("nonmembers.txt")
+	if os.IsNotExist(err2) {
+		f2, _ = os.Create("nonmembers.txt")
+	}
+	file2, _ := os.ReadFile("nonmembers.txt")
+	editor2.InsertText(string(file2))
+
+	defer f1.Close()
+	defer f2.Close()
+
 	wnd := g.NewMasterWindow("Live Shill Discriminator", 800, 800, g.MasterWindowFlagsFloating)
 	wnd.Run(loop)
 
